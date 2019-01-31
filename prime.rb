1049936b80f41be5948725b1df5ad58e8f1b14d8
Jason Fournier <jmf4nier@hotmail.com>
2019-01-31 16:07:24 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -6,9 +6,11 @@ def first_primes(n)
 def prime?(n)  
   
   if n < 1
-    p false
+    puts "not prime"
+    return false
   elsif n == 1 || n == 2 || n == 3    #covers for negatives and 1,2,3(first primes)
-    p true
+    puts "prime"
+    return true
   else 
       for x in 2..(n -1)
         new = [n % x]
