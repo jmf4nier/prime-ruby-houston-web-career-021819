497ef3cf00e47365f67e6d1081fcc4d8076ecf00
Jason Fournier <jmf4nier@hotmail.com>
2019-01-31 16:03:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,7 @@
 
 
 def first_primes(n)
-  if n < 1
-    p false
-  elsif n == 1 || n == 2 || n == 3    #covers for negatives and 1,2,3(first primes)
-    p true
-  else 
-    p false
-  end
-end
+  
 
 def prime?(n)  
   
