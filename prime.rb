e50189d1486b5c8f8100f52f3804ed5de9644d99
Jason Fournier <jmf4nier@hotmail.com>
2019-01-31 16:12:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -14,17 +14,16 @@ def prime?(n)
   else 
       for x in 2..(n -1)
         new = [n % x]
-        if new.include?(0)
-          puts "not prime"
-          return false
-        elsif 
-          (x == n-1) && new.include?(0) == false
-          puts "prime"
-          return true
-        end
+          if new.include?(0)
+           puts "not prime"
+            return false
+         elsif 
+            (x == n-1) && new.include?(0) == false
+           puts "prime"
+           return true
+         end
       end
   end
-
 end
 prime?(6)
 
